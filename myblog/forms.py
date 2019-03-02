b4fb9b4ce2a0feacb430a7218986a14d161b45b5
aniket <bruces1998@gmail.com>
2019-03-02 19:48:46 +0530

    College Blog using Django

---
+++ myblog/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from myblog.models import Comments,Post
+
+class PostForm(forms.ModelForm):
+
+
+
+    class Meta():
+        model = Post
+        fields = ('author','title','text')
+
+        widgets = {
+            'title' :forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
+
+class CommentForm(forms.ModelForm):
+
+
+        class Meta():
+
+            model = Comments
+            fields = ('author','text')
+            widgets = {
+                'author':forms.TextInput(attrs={'class':'textinputclass'}),
+                'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
+            }
